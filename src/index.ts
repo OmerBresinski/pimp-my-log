15ef923ecac69f998377be22b5aee62daca26cba
Yahel Golan <yahelg@post.bgu.ac.il>
2024-02-05 17:33:42 +0200

    Now supports multiple player enhancement and spreads the increased damage over a provided list of other players (no list of spells required for these players)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { input } from "@inquirer/prompts";
 import { readFileSync, writeFileSync } from "fs";
 let totalDmgAdded = 0;
-let dmgToSubtract = 0;
 let file: string;
 let fileName: string;
+let linesToSubtract = 0;
+let isDoneSubtracting = false;
 
 const askQuestions = async () => {
   const filePath = await input({ message: "Enter the file path: " });
@@ -35,19 +36,46 @@ const filterByBossAndPlayer = (
 
 const applyDmgModifier = (
   lines: Array<{ index: number; line: string }>,
-  modifier: number
+  modifier: number,
+  remainder: number
 ) => {
   console.log("Entered applyDmgModifier function");
-  //console.log(lines);
+  if (lines.length == 0) {
+    console.log("0 lines modified");
+    return lines;
+  }
   return lines.map(({ line, index }) => {
     const lineElements = line.split(",");
+
     const dmgValue = lineElements[28];
 
     if (+dmgValue) {
-      let dmgToAdd = (Number(dmgValue) * (1 + modifier / 100)).toFixed(0);
+      let dmgToAdd = "";
+      if (modifier > 0){
+        dmgToAdd = (Number(dmgValue) * (1 + modifier / 100)).toFixed(0);
+      } else {
+        if (remainder < 2 * modifier){ // both negative
+          console.log("remainder: " + remainder);
+          remainder -= modifier;
+          dmgToAdd = String(Number(dmgValue) + modifier + modifier );
+          console.log("new remainder: " + remainder + ". dmgToAdd: " + dmgToAdd);
+        } else {
+          dmgToAdd = String(Number(dmgValue) + modifier + remainder );
+          remainder = 0;
+        }
+      }
+      
       lineElements[28] = `${dmgToAdd}`;
-      totalDmgAdded += (Number(dmgToAdd));
-      // console.log("dmg added: " + dmgToAdd + ". total: " + totalDmgAdded); this shows the added damage is too high
+      let increment = (Number(dmgToAdd)- Number(dmgValue))
+      totalDmgAdded += increment;
+      if (totalDmgAdded == 0){ isDoneSubtracting = true; return { index, line: lineElements.join(",") };} 
+      if (totalDmgAdded < 0 ) {
+        lineElements[28] = `${Number(dmgToAdd)+Math.abs(totalDmgAdded)}`;
+        totalDmgAdded = 0;
+        isDoneSubtracting = true;
+        return { index, line: lineElements.join(",") }; 
+      }
+      //console.log("dmg added: " + increment + ". total: " + totalDmgAdded); 
     } else{
     //  console.log("error: " + dmgValue + " not dmgvalue");
     }
@@ -58,7 +86,6 @@ const applyDmgModifier = (
 
 const writeModifiedFile = (
   file: string,
-  answers: any,
   modifiedLines: Array<{ index: number; line: string }>
 ) => {
   
@@ -72,32 +99,62 @@ const writeModifiedFile = (
   file = readFileSync(fileName, "utf-8"); // TODO: Optimize such that the file variable is updated in the workspace instead of written and read from the OS
 };
 
+const modifyDmg = (
+  pNames: string,
+  sNames: string,
+  bossName: string,
+  dmgModifier: number,
+  remainder: number,
+  otherDPSNames: string,
+  modify: boolean,
+  done: boolean
+) => {
+  if (done) return;
+  let playerIndex = 0;
+  
+  pNames.split(";").forEach((name: string) => { 
+    sNames.split(";")[playerIndex].split(",").forEach((spell: string) => {
+      let lines = filterByBossAndPlayer(
+        file,
+        bossName,
+        name,
+        spell
+      );
+      console.log(name + " " + spell +"\nnum lines: " + lines.length);
+      if (modify){
+        if (dmgModifier < 0){
+          done = true
+          console.log("negative modifier: " + dmgModifier + ". remainder: " + remainder);
+        }
+        let modifiedLines = applyDmgModifier(lines, dmgModifier, remainder);
+
+        writeModifiedFile(file, modifiedLines);
+        console.log("Total dmg added after player " + name + " and spell " + spell + ": " + totalDmgAdded);
+      } else { // for dmg reduction, first only count the amount of lines, so we can calculate how much to subtract per line
+        linesToSubtract += lines.length;
+      }
+    });
+    if (dmgModifier > 0){ // generic list for otherDPSNames
+      playerIndex++;
+    }
+  });
+  if (!isDoneSubtracting){
+    let modifier: number = -1;
+    if (linesToSubtract < totalDmgAdded){
+      modifier = - Number((totalDmgAdded / linesToSubtract).toFixed(0))
+      remainder = - (totalDmgAdded % linesToSubtract);
+    } 
+    modifyDmg(otherDPSNames, "SWING_DAMAGE_LANDED,SPELL_DAMAGE", bossName, modifier, remainder, otherDPSNames, !modify, done)
+  }
+}
 async function main() {
   const answers = await askQuestions();
   file = readFileSync(answers.filePath, "utf-8");
   fileName = (String(answers.filePath.split("/").pop()));
   const currentDate = new Date();
   fileName = fileName.replace(".txt", "_modified"+currentDate.getTime()+".txt");
-  let playerIndex = 0;
-  answers.playerNames.split(";").forEach((name) => { 
-    answers.spellNames.split(";")[playerIndex].split(",").forEach((spell) => {
-      let lines = filterByBossAndPlayer(
-        file,
-        answers.bossName,
-        name,
-        spell
-      );
-      let modifiedLines = applyDmgModifier(lines, Number(answers.dmgModifier));
-    
-      writeModifiedFile(file, answers, modifiedLines);
-      console.log("Total dmg added after player " + name + " and spell " + spell + ": " + totalDmgAdded);
-    });
-    playerIndex++;
-  });
 
-  // TODO: add function to reduce damage. Probably won't know in advance by how much we're subtracting each time unless we send specific spells per player (don't wanna do that)
-
-
+  modifyDmg(answers.playerNames, answers.spellNames, answers.bossName,Number(answers.dmgModifier), 0, answers.otherDPSNames, true, false);
 
   console.log("Modified log file created successfully");
 }
